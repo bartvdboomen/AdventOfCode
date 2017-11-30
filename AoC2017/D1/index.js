0cb13ccb6d775718fed4f1438a888b6651167211
Bart van den Boomen <bartb@live.nl>
2017-11-30 19:28:18 +0100

    Prep

---
+++ AoC2017/D1/index.js
@@ -0,0 +1,8 @@
+﻿'use strict';
+
+let fs = require('fs');
+let path = require('path');
+let _ = require('lodash');
+let utils = require('./../helper');
+
+console.log('Hello world2');
---
+++ AoC2017/helper/index.js
@@ -0,0 +1,26 @@
+﻿var path = require('path');
+var process = require('process');
+var fs = require('fs');
+var _ = require('lodash');
+
+let service = {
+    readInput: (fileName) => {
+        return fs.readFileSync(path.join(service.getDayPath(), fileName || 'input'), { encoding: 'utf-8' });
+    },
+    readCommaSeperatedInput: (fileName) => {
+        return _.map(service.readInput(fileName).split(/\r\n|\n|\r/), (value) => {
+            return value.trim();
+        });
+    },
+    readNewLineSeperatedInput: (fileName) => {
+        return _.map(service.readInput(fileName).split(','), (value) => {
+            return value.trim();
+        });
+    },
+    getDayPath: () => {
+        let day = ('0' + process.argv[2]).substr(-2);
+        return path.join(process.cwd(), day);
+    }
+};
+
+module.exports = service;
