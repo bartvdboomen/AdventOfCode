0dd2df38eb2325604bc497ecafa7236afa8540ac
Bart van den Boomen <bartb@live.nl>
2017-12-01 12:27:00 +0100

    D1

--- AoC2017/D1/index.js
+++ AoC2017/D1/index.js
@@ -5,5 +5,24 @@ let path = require('path');
 let _ = require('lodash');
 let helper = require('./../helper');
 
-console.log('Hello world2');
+let input = helper.readInput(__dirname + '\\input.txt');
 
+let total = 0;
+let lastdigit = -1;
+let currentdigit = 0;
+let nextdigit = 0;
+for (let i = 0, x = input.length; i < x; i++) {
+    currentdigit = parseInt(input.charAt(i));
+    nextdigit = i + (x / 2);
+
+    if (nextdigit > x-1)
+        nextdigit = nextdigit - x;
+
+    nextdigit = input.charAt(nextdigit);
+
+    if (currentdigit == nextdigit)
+        total += currentdigit;
+
+    lastdigit = currentdigit;
+}
+console.log(total);
